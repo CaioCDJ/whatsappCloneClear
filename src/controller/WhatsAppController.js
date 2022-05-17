440c8c1d6ec4db53c58c058a16948fd02ffe0dde
CaioCDJ <caiodjesus1@hotmail.com>
2022-05-17 14:58:36 -0300

    Send Documents

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,6 @@
 import {Format} from './../util/Format';
 import {CameraController} from './CameraController';
+import { DocumentPreviewController } from './DocumentPreviewController';
 
 export class WhatsAppController{
 
@@ -200,11 +201,35 @@ export class WhatsAppController{
 
         this.el.btnClosePanelCamera.on('click',e=>{
             
+            this.closeAllMainPanel();
             this.el.panelMessagesContainer.show();
-            this.el.panelCamera.removeClass('open');
+            this._camera.stop();
         })
         this.el.btnTakePicture.on('click',e=>{
-            console.log('x');
+           
+            let dataUrl = this._camera.takePicture();
+            
+            this.el.pictureCamera.src = dataUrl;
+            
+            this.el.pictureCamera.show();
+            this.el.videoCamera.hide();
+            this.el.btnReshootPanelCamera.show();
+            this.el.containerTakePicture.hide();
+            this.el.containerSendPicture.show();   
+        })
+
+        this.el.btnReshootPanelCamera.on('click',e=>{
+            
+            this.el.pictureCamera.hide();
+            this.el.videoCamera.show();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.containerTakePicture.show();
+            this.el.containerSendPicture.hide();   
+        })
+
+        this.el.btnSendPicture.on('click',e=>{
+
+            console.log(this.el.pictureCamera.src);
         })
 
         this.el.btnAttachDocument.on('click',e=>{
@@ -214,14 +239,58 @@ export class WhatsAppController{
             this.el.panelDocumentPreview.css({
                 'height':'100%'
             })
+            this.el.inputDocument.click();
         })
 
-        this.el.btnClosePanelDocumentPreview.on('click',e=>{
+        this.el.inputDocument.on('change', e =>{
 
-            this.closeAllMainPanel();
-            this.el.panelMessagesContainer.show();
-        });
+            if(this.el.inputDocument.files.length){
 
+                let file = this.el.inputDocument.files[0];
+
+                this._documentPreviewController = new DocumentPreviewController(file);
+
+                this._documentPreviewController.getPreviewData().then(result=>{
+
+                this.el.imgPanelDocumentPreview.src = result.src;
+                this.el.infoPanelDocumentPreview.innerHTML = result.info;
+                this.el.imagePanelDocumentPreview.show();
+                this.el.filePanelDocumentPreview.hide();
+
+                
+                }).catch(err=>{
+
+                    console.log(file.type);
+
+                    switch (file.type) {
+                        // word
+                        case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                        case 'application/msword':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-doc';
+                        break;
+                        
+                        // excel
+                        case'application/vnd.ms-excel.sheet.macroEnabled.12':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-xls';
+                        break;
+
+                        // presentation
+                        case'application/vnd.ms-powerpoint':
+                        case'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-ppt';
+                        break;
+
+                        default:
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic';
+                        break;
+                    }
+                    this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+                    this.el.imagePanelDocumentPreview.hide();
+                    this.el.filePanelDocumentPreview.show();
+                })
+            }
+        })
+    
         this.el.btnSendDocument.on('click',e=>{
             console.log('doc enviado');
         })
